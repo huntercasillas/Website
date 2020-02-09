d35928ba1999363f724b729bdc761fa13add8df9
Hunter Casillas <huntercasillas@me.com>
2020-02-09 14:41:02 -0700

    Fixed mobile portfolio overlays

--- js/scripts.js
+++ js/scripts.js
@@ -133,15 +133,27 @@ $(function() {
                 if (this.hash.slice(1) === 'portfolio') {
                     $('html,body').animate({
                         scrollTop: target.offset().top
-                    }, 900);
+                    }, 1000);
                 } else if (this.hash.slice(1) === 'intro') {
                     $('html,body').animate({
                         scrollTop: target.offset().top
                     }, 1500);
+                } else if (this.hash.slice(1) === 'mountains') {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1500);
+                } else if (this.hash.slice(1) === 'design') {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1900);
+                } else if (this.hash.slice(1) === 'contact') {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 2050);
                 } else {
                     $('html,body').animate({
                         scrollTop: target.offset().top
-                    }, 1800);
+                    }, 2050);
                 }
 				return false;
 			}
@@ -157,39 +169,159 @@ document.querySelector("#nav-toggle").addEventListener("click", function() {
 });
 
 /***************** Overlays ******************/
-overlayOpen = false;
+var overlaysOpen = [false, false, false, false, false, false];
+var current = "none";
 
-$(document).ready(function(){
+function closeOverlay(name, index) {
+    if (current !== "none") {
+        var previous = $(name);
+//        previous.preventDefault();
+//        previous.stopPropagation();
+        if (previous.closest(".img").hasClass("hover")) {
+            previous.closest(".img").removeClass("hover");
+        }
+        current = "none";
+        overlaysOpen[index] = false;
+    }
+}
+
+$(document).ready(function() {
     if (Modernizr.touch) {
         // show the close overlay button
         $(".close-overlay").removeClass("hidden");
         // handle the adding of hover class when clicked
         $(".img").click(function(e){
-            if (overlayOpen === false) {
-                if (!$(this).hasClass("hover")) {
-                    $(this).addClass("hover");
+            if (current !== "none") {
+                // Close previously opened overlay
+                if (current === "#daisy-overlay") {
+                    closeOverlay(current, 0);
+                } else if (current === "#dragon-overlay") {
+                    closeOverlay(current, 1);
+                } else if (current === "#carbon-overlay") {
+                    closeOverlay(current, 2);
+                } else if (current === "#family-overlay") {
+                    closeOverlay(current, 3);
+                } else if (current === "#hoopr-overlay") {
+                    closeOverlay(current, 4);
+                } else if (current === "#tree-overlay") {
+                    closeOverlay(current, 5);
                 }
-                overlayOpen = true;
+            }
+            // Check which portfolio item was clicked
+            if (this.id === "daisy") {
+                if (overlaysOpen[0] === false) {
+                    if (!$(this).hasClass("hover")) {
+                        $(this).addClass("hover");
+                    }
+                    overlaysOpen[0] = true;
+                    current = "#daisy-overlay";
+                }
+            } else if (this.id === "dragon") {
+                if (overlaysOpen[1] === false) {
+                    if (!$(this).hasClass("hover")) {
+                        $(this).addClass("hover");
+                    }
+                    overlaysOpen[1] = true;
+                    current = "#dragon-overlay";
+                }
+            } else if (this.id === "carbon") {
+                if (overlaysOpen[2] === false) {
+                    if (!$(this).hasClass("hover")) {
+                        $(this).addClass("hover");
+                    }
+                    overlaysOpen[2] = true;
+                    current = "#carbon-overlay";
+                }
+            } else if (this.id === "family") {
+                if (overlaysOpen[3] === false) {
+                    if (!$(this).hasClass("hover")) {
+                        $(this).addClass("hover");
+                    }
+                    overlaysOpen[3] = true;
+                    current = "#family-overlay";
+                }
+            } else if (this.id === "hoop") {
+                if (overlaysOpen[4] === false) {
+                    if (!$(this).hasClass("hover")) {
+                        $(this).addClass("hover");
+                    }
+                    overlaysOpen[4] = true;
+                    current = "#hoop-overlay";
+                }
+            } else if (this.id === "tree") {
+                if (overlaysOpen[5] === false) {
+                    if (!$(this).hasClass("hover")) {
+                        $(this).addClass("hover");
+                    }
+                    overlaysOpen[5] = true;
+                    current = "#tree-overlay";
+                }
             }
         });
-        // close overlay if x button clicked
-        $(".close-overlay").click(function(e){
-            e.preventDefault();
-            e.stopPropagation();
-            if ($(this).closest(".img").hasClass("hover")) {
-                $(this).closest(".img").removeClass("hover");
-            }
-            overlayOpen = false;
-        });
+                  
         // close overlay if it is open
-        $(".overlay").click(function(e){
-            if (overlayOpen === true) {
-                e.preventDefault();
-                e.stopPropagation();
-                if ($(this).closest(".img").hasClass("hover")) {
-                    $(this).closest(".img").removeClass("hover");
+        $(".overlay").click(function(e) {
+            // Check which portfolio item was clicked
+            if (this.id === "daisy-overlay") {
+                if (overlaysOpen[0] === true) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    if ($(this).closest(".img").hasClass("hover")) {
+                        $(this).closest(".img").removeClass("hover");
+                    }
+                    current = "none";
+                    overlaysOpen[0] = false;
                 }
-                overlayOpen = false;
+            } else if (this.id === "dragon-overlay") {
+                if (overlaysOpen[1] === true) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    if ($(this).closest(".img").hasClass("hover")) {
+                        $(this).closest(".img").removeClass("hover");
+                    }
+                    current = "none";
+                    overlaysOpen[1] = false;
+                }
+            } else if (this.id === "carbon-overlay") {
+                if (overlaysOpen[2] === true) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    if ($(this).closest(".img").hasClass("hover")) {
+                        $(this).closest(".img").removeClass("hover");
+                    }
+                    current = "none";
+                    overlaysOpen[2] = false;
+                }
+            } else if (this.id === "family-overlay") {
+                if (overlaysOpen[3] === true) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    if ($(this).closest(".img").hasClass("hover")) {
+                        $(this).closest(".img").removeClass("hover");
+                    }
+                    current = "none";
+                    overlaysOpen[3] = false;
+                }
+            } else if (this.id === "hoop-overlay") {
+                if (overlaysOpen[4] === true) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    if ($(this).closest(".img").hasClass("hover")) {
+                        $(this).closest(".img").removeClass("hover");
+                    }
+                    current = "none";
+                    overlaysOpen[4] = false;
+                }
+            } else if (this.id === "tree-overlay") {
+                if (overlaysOpen[5] === true) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    if ($(this).closest(".img").hasClass("hover")) {
+                        $(this).closest(".img").removeClass("hover");
+                    }
+                    current = "none";
+                    overlaysOpen[5] = false;
+                }
             }
         });
     } else {
